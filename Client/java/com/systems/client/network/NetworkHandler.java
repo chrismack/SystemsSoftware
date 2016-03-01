6a43b6ef8d674d3a42e7c800ed6c48c06acf3a1f
Benjamin Doe <n0561281@MAE203-16-linux.ads.ntu.ac.uk>
2016-03-01 14:56:05 +0000

    Attempted to make a start on the client. Still getting my head around their code.

--- Client/java/com/systems/client/network/NetworkHandler.java
+++ Client/java/com/systems/client/network/NetworkHandler.java
@@ -1,13 +1,38 @@
 package com.systems.client.network;
 
+
+import java.net.ServerSocket;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class NetworkHandler
 {
-
+	/*
+	 * I am assuming this has to match up with the server hostname and port.
+	 */
+	private static String HOSTNAME = "localhost";
+	private static int PORT = 4556;
+	
+	/*
+	 * For multi-client handling
+	 * 
+	 *  private static int CLIENTID = 1;
+	 */
+	
 	private static NetworkHandler INSTANCE;
 	
 	public NetworkHandler()
 	{
 		INSTANCE = this;
+
+		ServerSocket socket = new ServerSocket(PORT);
+		try 
+		{
+			while(true)
+			{
+				
+			}
+		}
 	}
 	
 	
