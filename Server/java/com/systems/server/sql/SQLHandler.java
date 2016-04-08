f850aa4b442d9f16a4970c119b94a23ea8954ae3
Chris Mack <chris.mack@hotmail.co.uk>
2016-04-08 21:48:23 +0100

    Add create statments for sql

--- Server/java/com/systems/server/sql/SQLHandler.java
+++ Server/java/com/systems/server/sql/SQLHandler.java
@@ -1,5 +1,7 @@
 package com.systems.server.sql;
 
+import java.io.File;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -11,6 +13,13 @@ public class SQLHandler
 	private static SQLHandler INSTANCE;
 	
 	private Connection connection = null;
+	private String sqlCreateLikedMusic;
+	private String sqlCreateUsers;
+	private String sqlCreateUserFriends;
+	private String sqlCreateSongs;
+	private String sqlCreateUserPosts;
+	
+	
 	
 	public static SQLHandler getInstance()
 	{
@@ -24,10 +33,43 @@ public class SQLHandler
 	public SQLHandler(String dbPath)
 	{
 		INSTANCE = this;
+		File database =  new File(dbPath);
+		
+		boolean create = false;
+		
+		if(!database.exists())
+		{
+			try
+			{
+				database.createNewFile();
+				create = true;
+			}
+			catch (IOException e)
+			{
+				e.printStackTrace();
+			}
+			 sqlCreateLikedMusic  = "CREATE TABLE 'LikedMusic' ('username'	INTEGER,'genre'	INTEGER,PRIMARY KEY(username,genre))";
+			 sqlCreateUsers	      = "CREATE TABLE Users('username'	TEXT NOT NULL UNIQUE,'password'	TEXT NOT NULL,'dateOfBirth'	TEXT NOT NULL,'placeOfBirth'TEXT NOT NULL,'profilePic'	"
+										+ "TEXT,'ip'	TEXT,PRIMARY KEY(username))";
+			 sqlCreateUserFriends = "CREATE TABLE UserFriends ('username'	TEXT,'friendUsername'TEXT,'pending'TEXT,PRIMARY KEY(username,friendUsername))";
+			 sqlCreateSongs 	  = "CREATE TABLE 'Songs' ('username'	TEXT,'songTitle'	TEXT,PRIMARY KEY(username,songTitle))";
+			 sqlCreateUserPosts   = "CREATE TABLE UserPost ('username'	TEXT,'time'	INTEGER,'post'	TEXT,'ID'	INTEGER,PRIMARY KEY(ID))";
+		}
+		
 	    try 
 	    {
 	      Class.forName("org.sqlite.JDBC");
 	      connection = DriverManager.getConnection("jdbc:sqlite:" + dbPath);
+	      if(create)
+	      {
+	    	  connection.createStatement().executeUpdate(sqlCreateLikedMusic);
+	    	  connection.createStatement().executeUpdate(sqlCreateUsers);
+	    	  connection.createStatement().executeUpdate(sqlCreateUserFriends);
+	    	  connection.createStatement().executeUpdate(sqlCreateSongs);
+	    	  connection.createStatement().executeUpdate(sqlCreateUserPosts);
+	    	  if(!connection.getAutoCommit())
+	    		  connection.commit();
+	      }
 	    }
 	    catch (Exception e)
 	    {
